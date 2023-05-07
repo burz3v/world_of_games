3df973bf943c454e21655fa3ec13792c3e819ace
burz3v <immunitet92@gmail.com>
2023-05-07 23:53:47 +0300

    docker-compose and e2e fixes

--- tests/e2e.py
+++ tests/e2e.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 
 def test_scores_service():
     my_driver = webdriver.Chrome(executable_path="chromedriver.exe")
-    my_driver.get("http://127.0.0.1:5000/")
+    my_driver.get("http://localhost:8777/")
     score = my_driver.find_element(By.ID, "score").text
     if 1 <= int(score) <= 1000:
         return True
